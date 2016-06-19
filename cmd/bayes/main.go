4ffb8c45b9d0a3fed3a8d69e0a57a4cb78a65bb3
Patrice Ferlet <metal3d@gmail.com>
2016-06-19 13:50:57 +0200

    Renamed vars to be more explicit

--- cmd/bayes/main.go
+++ cmd/bayes/main.go
@@ -20,17 +20,17 @@ func main() {
 		file     string
 		content  string
 		lang     string
-		get      bool
+		check    bool
 		auto     float64
-		all      bool
+		checkAll bool
 	)
 
 	flag.StringVar(&category, "category", "", "category name")
 	flag.StringVar(&file, "save", "bayes.json", "file base")
 	flag.StringVar(&content, "content", "", "message to train")
 	flag.StringVar(&lang, "lang", "", "language for stemming, if not set the content will not be stemmed")
-	flag.BoolVar(&get, "check", false, "check result")
-	flag.BoolVar(&all, "check-all", false, "check result in each categories")
+	flag.BoolVar(&check, "check", false, "check result")
+	flag.BoolVar(&checkAll, "check-all", false, "check result in each categories")
 	flag.Float64Var(&auto, "save-if", 0.0, "if > 0, save the result in found category if bayes result is greater than this value")
 	flag.Parse()
 
@@ -39,22 +39,29 @@ func main() {
 	}
 
 	category = strings.TrimSpace(category)
-	if category == "" && !all {
-		if !get {
+	if category == "" && !checkAll {
+		if !check {
 			log.Fatal("You must provide a category name to train")
 		} else {
 			log.Fatal("You must provide a category name or -check-all to test the whole categories")
 		}
 	}
 
+	// try to open the saved file
 	f, _ := os.Open(file)
+
+	// the current category
+	c := &bayes.Category{}
 
 	// parse json file
 	d := json.NewDecoder(f)
 
+	// The entire categories - try to get
+	// them from the saved file
 	categories := []*bayes.Category{}
 	d.Decode(&categories)
 
+	// if a lang is given, stem content
 	if lang != "" {
 		words := bayes.Split(content)
 		content = ""
@@ -68,9 +75,10 @@ func main() {
 		content = strings.TrimSpace(content)
 	}
 
-	c := &bayes.Category{}
+	// if a category is given, try to find it from
+	// the categories list
 	if category != "" {
-		if found := getCat(category, categories); found != nil {
+		if found := getCategoryByName(category, categories); found != nil {
 			c = found
 		} else {
 			c = bayes.NewCategory(category)
@@ -78,8 +86,10 @@ func main() {
 		}
 	}
 
-	if get || all {
-		if !all {
+	if check || checkAll {
+		// In case of user ask to check bayesian result
+		if !checkAll {
+			// Simple check
 			b := bayes.Bayes(content, c, categories)
 			fmt.Println(b)
 
@@ -88,6 +98,7 @@ func main() {
 				save(categories, file)
 			}
 		} else {
+			// Check for all categories, find the best
 			max := -1.0
 			best := &bayes.Category{}
 			results := map[string]float64{}
@@ -103,18 +114,21 @@ func main() {
 				fmt.Println(n, r)
 			}
 
+			// user asked to save the test case to the
+			// categories file
 			if auto > 0 && max > auto {
 				best.Train(content)
 				save(categories, file)
 			}
 		}
 	} else {
+		// else, user want to train
 		c.Train(content)
 		save(categories, file)
 	}
 }
 
-func getCat(name string, ctg []*bayes.Category) *bayes.Category {
+func getCategoryByName(name string, ctg []*bayes.Category) *bayes.Category {
 	for _, c := range ctg {
 		if c.Name == name {
 			return c
